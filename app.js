953cc210f0bab160946c4863da6edfcc6eb39d12
sebastianosalzillo <salzillosebastiano@gmail.com>
2024-11-28 14:15:41 +0100

    feat: esercizio base completato

---
+++ app.js
@@ -0,0 +1,11 @@
+const parolaPassata = process.argv[2]
+const saluto2 = `hello ${parolaPassata}`
+
+console.log(saluto2) 
+
+const name = "Boolean" 
+const saluto = `Hello ${name}`
+
+
+
+console.log(saluto)
